225f798013ebc36220ab573c1886b0f092f08aad
t-tullis <t.tullis34@gmail.com>
2019-07-08 14:55:29 -0700

    Reached MVP and test passed

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -15,7 +15,8 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [None] * capacity
 
 
 # '''
@@ -23,8 +24,11 @@ class BasicHashTable:
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
-
+    hashed_num = 5381
+   
+    for character in string:
+        hashed_num = (( hashed_num << 5) + hashed_num) + ord(character)
+    return hashed_num % (max - 1)
 
 # '''
 # Fill this in.
@@ -32,7 +36,16 @@ def hash(string, max):
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    key_value_pair = Pair(key, value)
+
+    hash_index = hash(key_value_pair.key, hash_table.capacity)
+    
+    if hash_table.storage[hash_index] is not None:
+        print(f"Overwriting index {hash_index} of hash table.")
+    
+    hash_table.storage[hash_index] = key_value_pair
+
+
 
 
 # '''
@@ -41,7 +54,13 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+   
+    hash_index = hash(key, hash_table.capacity)
+
+    if hash_table.storage[hash_index] is None:
+        print(f"{hash_index} value is None")
+    else:
+        hash_table.storage[hash_index] = None
 
 
 # '''
@@ -50,14 +69,46 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    hash_index = hash(key, hash_table.capacity)
+    if hash_table.storage[hash_index] is not None:
+        return hash_table.storage[hash_index].value
+    else:
+        return None
 
+
+# def insert_test():
+        #creates Hash Table stored under ht
+#     ht = BasicHashTable(16)
+
+#     hash_table_insert(ht, "line", "Yoooo")
+#     hash_table_insert(ht, "second", "This is the second value")
+        #Retrieves key "second" and prints the value stored.
+#     print(hash_table_retrieve(ht, "second"))
+
+# insert_test()
+
+# def remove_test():
+#     #creates Hash Table stored under ht
+#     ht = BasicHashTable(16)
+
+#     #inserts key/value pair into ht(hashtable)
+#     hash_table_insert(ht, "line", "Yoooo")
+#     hash_table_insert(ht, "second", "This is the second value")
+
+#     #removes key "second" from ht(hashtable)
+#     hash_table_remove(ht, "second")
+
+#     #attempts to retrieve removed key from ht(hashtable) and returns None
+#     print(hash_table_retrieve(ht, "second"))
+
+# remove_test()
 
 def Testing():
     ht = BasicHashTable(16)
 
     hash_table_insert(ht, "line", "Here today...\n")
 
+    
     hash_table_remove(ht, "line")
 
     if hash_table_retrieve(ht, "line") is None:
@@ -67,3 +118,4 @@ def Testing():
 
 
 Testing()
+
